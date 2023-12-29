74479c4731bc412313e101e761573060489db99e
Ellery Zhang <allotory@msn.com>
2023-12-29 10:26:31 +0800

    build: init project with spring boot

---
+++ src/main/java/com/philodic/PhilodicApplication.java
@@ -0,0 +1,13 @@
+package com.philodic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilodicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilodicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/philodic/PhilodicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philodic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilodicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
